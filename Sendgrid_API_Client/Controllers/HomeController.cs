fcbbd606b212f2b0961c27d45aba696081f4f32c
tahirazeemalvi <engtahiralvi@gmail.com>
2020-10-27 23:16:32 +0500

    Add project files.

---
+++ Sendgrid_API_Client/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Sendgrid_API_Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string _apiKey = "SENDGRID_API_KEY_HERE";
+        private static readonly HttpClient httpClient = new HttpClient();
+        public async Task<ActionResult> Index()
+        {
+            var client = new SendGridClient(httpClient, new SendGridClientOptions { ApiKey = _apiKey, HttpErrorAsException = true });
+
+            var from = new EmailAddress("test@example.com", "Example User");
+            var subject = "Hello World from the Twilio SendGrid CSharp Library Helper!";
+            var to = new EmailAddress("engtahiralvi@gmail.com", "Tahir Alvi");
+            var plainTextContent = "Hello, Email from the helper [SendSingleEmailAsync]!";
+            var htmlContent = "<strong>Hello, Email from the helper! [SendSingleEmailAsync]</strong>";
+
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+            ViewBag.responseCode = response.StatusCode;
+            return View();
+        }
+    }
+}
---
+++ Sendgrid_API_Client/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sendgrid_API_Client.MvcApplication" Language="C#" %>
